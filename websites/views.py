0ebaf969caf384be266bd7cb11a9efbed1e929b7
iamgolden55 <eruwagolden55@gmail.com>
2024-04-16 15:40:47 +0100

    Update links in hotels-card.html and nav.html templates

--- websites/views.py
+++ websites/views.py
@@ -16,4 +16,9 @@ def home():
 def profile():
     hotels_with_cities = db.session.query(Hotel, City).join(City, Hotel.city_id == City.id).all()
     return render_template('profile.html', user=current_user, hotels_with_cities=hotels_with_cities, rooms=Room.query.all(), bookings=Booking.query.all())
-    
+
+@views.route('/bookings/<int:hotel_id>')
+@login_required
+def bookings(hotel_id):
+    hotel_with_city = db.session.query(Hotel, City).join(City, Hotel.city_id == City.id).filter(Hotel.id == hotel_id).first()
+    return render_template('bookings.html', user=current_user, hotel_with_city=hotel_with_city, rooms=Room.query.all(), bookings=Booking.query.all())
